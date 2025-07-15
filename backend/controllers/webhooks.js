5744942221c7fbb095fd455776a50f86d5f46bd9
NithiyasriGSS2006 <nithiyasrigss@gmail.com>
2025-07-15 18:50:31 +0530

    corrected user

--- backend/controllers/webhooks.js
+++ backend/controllers/webhooks.js
@@ -17,7 +17,7 @@ export const clerkWebhooks = async (req,res)=>{
         case 'user-created':{
             const userData ={
                 id:data.id,
-                email:data.email_address[0].email_address,
+                email:data.email_addresses[0].email_address,
                 name: data.first_name +" "+ data.last_nam0e,
                 imageUrl: data.image_url,
             }
