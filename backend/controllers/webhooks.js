8be3f6d27d09c75c6c3238bf668d93886abfd4c3
NithiyasriGSS2006 <nithiyasrigss@gmail.com>
2025-07-15 19:11:54 +0530

    corrected errors

--- backend/controllers/webhooks.js
+++ backend/controllers/webhooks.js
@@ -14,13 +14,27 @@ export const clerkWebhooks = async (req,res)=>{
     const {data,type} = req.body
     switch(type)
     {
-        case 'user-created':{
+        case 'user.created':{
             const userData ={
-                id:data.id,
+                _id:data.id,
                 email:data.email_addresses[0].email_address,
-                name: data.first_name +" "+ data.last_nam0e,
+                name: data.first_name +" "+ data.last_name,
                 imageUrl: data.image_url,
             }
+            await User.create(userData)
+             res.json({})
+             break;
+        }
+           
+           
+           
+        case 'user.updated':{
+            const userData ={
+        
+                email:data.email_addresses[0].email_address,
+                name: data.first_name +" "+ data.last_name,
+                imageUrl: data.image_url,
+            }   
             await User.findByIdAndUpdate(data.id,userData)
              res.json({})
              break;
